019af84f4d945828f055ec786484cafeda11e0d7
ddb <81467937+Kim-ge@users.noreply.github.com>
2024-08-19 08:22:18 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,8 +1,14 @@
 def solution(word):
     letters = ['A', 'E', 'I', 'O', 'U']
-    position_weight = [781, 156, 31, 6, 1]  # 각 자리별 가중치
     result = 0
-
+    
+    # 각 자리수별 가중치를 계산
+    position_weight = []
+    for i in range(5):
+        weight = sum(5 ** j for j in range(5 - i))
+        position_weight.append(weight)
+    
+    # 입력된 word에 대해 순서 계산
     for i, char in enumerate(word):
         index = letters.index(char)
         result += index * position_weight[i] + 1
