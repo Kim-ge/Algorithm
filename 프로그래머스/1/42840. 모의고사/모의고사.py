a4f05eae06de016357a82aa1e5fbdf26e89d4609
ddb <81467937+Kim-ge@users.noreply.github.com>
2025-10-31 15:08:02 +0900

    [level 1] Title: 모의고사, Time: 4.27 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,28 +1,19 @@
 def solution(answers):
-    #각 수포자의 답안 패턴 정의
-    pattern1 = [1, 2, 3, 4, 5]
-    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = []
+    score=[0,0,0]
+    p1 = [1,2,3,4,5] #5개
+    p2 = [2,1,2,3,2,4,2,5] #8개
+    p3 = [3,3,1,1,2,2,4,4,5,5] #10개
     
-    #각 수포자가 맞힌 문제 수를 저장할 리스트
-    scores = [0, 0, 0]
-    
-    #정답과 각 패턴을 비교하여 맞힌 문제 수 계산
-    for i, answer in enumerate(answers):
-        if answer == pattern1[i % len(pattern1)]:
-            scores[0] += 1
-        if answer == pattern2[i % len(pattern2)]:
-            scores[1] += 1
-        if answer == pattern3[i % len(pattern3)]:
-            scores[2] += 1
-    
-    #가장 높은 점수 계산
-    max_score = max(scores)
-    
-    #가장 높은 점수를 받은 수포자(들)을 찾기
-    result = []
+    for i in range(len(answers)):
+        if answers[i]==p1[i%5]:
+            score[0]+=1
+        if answers[i]==p2[i%8]:
+            score[1]+=1
+        if answers[i]==p3[i%10]:
+            score[2]+=1
+            
     for i in range(3):
-        if scores[i] == max_score:
-            result.append(i + 1)
-    
-    return result
+        if score[i]==max(score):
+            answer.append(i+1)
+    return answer
