b85e580ea80b716f57310a8362bb3cda6cc3d765
ddb <81467937+Kim-ge@users.noreply.github.com>
2025-11-01 15:44:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.14 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,10 @@
 from collections import Counter
 
 def solution(participant, completion):
-    counter1 = Counter(participant)
-    counter2 = Counter(completion)
+    answer = ''
+    p_count=Counter(participant)
+    c_count=Counter(completion)
     
-    answer = []
-    
-    for element in counter1:
-        if element not in counter2:
-            answer.append(element)
-        elif counter1[element] > counter2[element]:
-            answer.append(element)
-    return ', '.join(answer)
+    answer = p_count - c_count
+
+    return list(answer.keys())[0]
