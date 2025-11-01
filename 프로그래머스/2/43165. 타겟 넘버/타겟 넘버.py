108efac7f490e8092f4433dd5e6977c3c42cacea
ddb <81467937+Kim-ge@users.noreply.github.com>
2025-11-01 16:44:14 +0900

    [level 2] Title: 타겟 넘버, Time: 361.11 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,16 @@
-def solution(numbers, target):
-    def dfs(index, current_sum):
-        #모든 숫자를 탐색한 경우
-        if index == len(numbers):
-            #타겟 넘버와 현재 합이 같은 경우
-            return 1 if current_sum == target else 0
-        
-        #현재 숫자를 더하거나 빼는 두 가지 경우에 대해 재귀적으로 탐색
-        return dfs(index + 1, current_sum + numbers[index]) + dfs(index + 1, current_sum - numbers[index])
+answer=0
+
+def dfs(numbers,target, current, idx):
+    global answer
     
-    # DFS 탐색 시작
-    return dfs(0, 0)
+    if len(numbers)==idx:
+        if target==current:
+            answer+=1
+        return
+    else:
+        dfs(numbers,target,current+numbers[idx],idx+1)
+        dfs(numbers,target,current-numbers[idx],idx+1)
+
+def solution(numbers, target):
+    dfs(numbers,target,0,0)
+    return answer
